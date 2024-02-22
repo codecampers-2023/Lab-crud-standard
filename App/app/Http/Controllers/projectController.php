71025a0e289868ac3272ca43b2e5d7b48be6f062
Reda Grain <grain.reda.solicode@gmail.com>
2024-02-22 14:13:19 +0100

    reda grain

--- App/app/Http/Controllers/projectController.php
+++ App/app/Http/Controllers/projectController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 
 class projectController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $projectrepository = new \App\Repository\ProjectRepository();
         $totalprojects = $projectrepository->count();
         return view('index', compact('totalprojects'));
     }
 
-    public function create(Request $request){
-        $add_project = new \App\Repository\ProjectRepository();
-        $add_project->create($request->all());
-        return view('index'); 
+    public function create(Request $request)
+    {
+        $projectrepository = new \App\Repository\ProjectRepository();
+        $projectrepository->create($request->all());
+        $totalprojects = $projectrepository->count();
+        return view('index', compact('totalprojects'));
     }
 }
