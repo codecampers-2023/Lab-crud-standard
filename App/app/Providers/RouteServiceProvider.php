aabdf421b374fcc2aadca6ac8899f7c47a15e447
ADNANLH <adnanharrak5@gmail.com>
2024-03-01 16:40:13 +0100

    71-fix-init-app

--- App/app/Providers/RouteServiceProvider.php
+++ App/app/Providers/RouteServiceProvider.php
@@ -36,13 +36,6 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
-
-                Route::middleware('web')
-                ->group(base_path('routes/projets/projet.php'));
-
-
-                Route::middleware('web')
-                ->group(base_path('routes/taches/tache.php'));
         });
     }
 }
