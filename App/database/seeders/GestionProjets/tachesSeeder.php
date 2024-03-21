6b6cf61dc508a66ca1dd4bac1395e273a125e195
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-03-21 14:45:41 +0000

    update taskfrontend

--- App/database/seeders/GestionProjets/tachesSeeder.php
+++ App/database/seeders/GestionProjets/tachesSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders\taches;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use App\Models\taches\tache;
+use App\Models\GestionProjets\Task;
 
 
 class TachesSeeder extends Seeder
@@ -45,7 +45,7 @@ class TachesSeeder extends Seeder
 
         ];
          foreach ($tachesData as $tacheData) {
-            Tache::create($tacheData);
+            Task::create($tacheData);
         }
     }
 }
