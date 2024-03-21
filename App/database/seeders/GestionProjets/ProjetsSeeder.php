72f471a927e6c4cc7504a915dadac02158218f97
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-03-21 14:44:27 +0000

    update projetfrontend

--- App/database/seeders/GestionProjets/ProjetsSeeder.php
+++ App/database/seeders/GestionProjets/ProjetsSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders\projets;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\projets\projet;
+use App\Models\GestionProjets\Projet;
 
 class ProjetsSeeder extends Seeder
 {
