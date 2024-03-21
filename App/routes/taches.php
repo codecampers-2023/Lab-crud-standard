3eefc0e29b641b4126f5ce3fd9dfc0af5a117205
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-03-21 13:42:59 +0000

    update init

--- App/routes/taches.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\taches\TacheController;
-
-
-Route::get('projects/tasks/{projetId}',[TacheController::class,'index'])->name('projects.tasks');
-
-Route::resource('tasks', TacheController::class);
